e54eb25dd23b2b36c999c1ebf1bfe81e67156c26
Chanakya <ucs3721@outlook.com>
2022-01-15 12:06:20 +0530

    Migrate to Nest.js (#1)
    * Add nestjs packages
    
    * init nestjs template
    
    * better understanding of nestjs. Time to go full speed.
    
    * updates, i guess

---
+++ packages/fastify-api/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from "@nestjs/core";
+import { Module } from "@nestjs/common";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+
+import { LinksController } from "./controllers/links.controller";
+
+@Module({
+	imports: [],
+	controllers: [LinksController],
+})
+class AppModule {}
+
+(async () => {
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+	await app.listen(3100);
+	console.log(`Application is running on: ${await app.getUrl()}`);
+})();
