b9e585d5ddf15cda7519ee0c6a027c97808fa76a
Chanakya <ucs3721@outlook.com>
2022-01-15 19:24:06 +0530

    add `fastify-helmet` and CORS

--- packages/fastify-api/src/main.ts
+++ packages/fastify-api/src/main.ts
@@ -2,17 +2,23 @@ import { NestFactory } from "@nestjs/core";
 import { Module } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import fastifyHelmet from "fastify-helmet";
 
 import { LinksController } from "./links/links.controller";
 
 @Module({
 	imports: [],
 	controllers: [LinksController],
+	providers: [],
+	exports: [],
 })
 class AppModule {}
 
 (async () => {
 	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+	app.register(fastifyHelmet);
+	app.enableCors();
 
 	const config = new DocumentBuilder()
 		.setTitle("Clink OpenAPI Server")
@@ -20,7 +26,7 @@ class AppModule {}
 		.setLicense("MIT", "https://github.com/U-C-S/clink-node/blob/main/LICENSE")
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup("api", app, document);
+	SwaggerModule.setup("API", app, document);
 
 	await app.listen(3100);
 	console.log(`Server running on: ${await app.getUrl()}`);
