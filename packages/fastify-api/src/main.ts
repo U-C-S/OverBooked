6477e15cbfe792bab989dbc585cfa407e14f2a92
Chanakya <ucs3721@outlook.com>
2022-01-15 13:21:59 +0530

    Add swagger support

--- packages/fastify-api/src/main.ts
+++ packages/fastify-api/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { Module } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
-import { LinksController } from "./controllers/links.controller";
+import { LinksController } from "./links/links.controller";
 
 @Module({
 	imports: [],
@@ -12,6 +13,15 @@ class AppModule {}
 
 (async () => {
 	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+	const config = new DocumentBuilder()
+		.setTitle("Clink OpenAPI Server")
+		.setVersion("0.1")
+		.setLicense("MIT", "https://github.com/U-C-S/clink-node/blob/main/LICENSE")
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, document);
+
 	await app.listen(3100);
-	console.log(`Application is running on: ${await app.getUrl()}`);
+	console.log(`Server running on: ${await app.getUrl()}`);
 })();
