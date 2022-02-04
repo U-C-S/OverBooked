83413a03ebf35dc16d0ff6fc5ae85136639587b3
U-C-S <ucs3721@outlook.com>
2022-02-04 15:51:32 +0530

    Add `mantine` packages

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<MantineProvider>
+			<Component {...pageProps} />
+		</MantineProvider>
+	);
 }
-
-export default MyApp;
