541810bd25ebf5489ee4fe333ac3c0cf739ab96a
U-C-S <ucs3721@outlook.com>
2022-03-28 23:33:08 +0530

    EOD and For now, Implement all in a single component

--- apps/client/components/Layout/HomeLayout.component.tsx
+++ apps/client/components/Layout/HomeLayout.component.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Styles from "./HomeLayout.module.scss";
 import { Header } from "../Header";
+import { Button } from "@mantine/core";
 
 export function HomeLayout() {
 	return (
 		<div className={Styles.parent}>
-			<Header />
+			<header>
+				<div>OverBooked</div>
+				<div className={Styles.quickBarContainer}>
+					<div className={Styles.quickBar}>
+						<p>lol</p>
+						<Button type="button">Login</Button>
+					</div>
+				</div>
+			</header>
 		</div>
 	);
 }
