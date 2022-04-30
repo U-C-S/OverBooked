e39765ecd68d4b2ddfe0fb2bb72fed985bca83d4
U-C-S <ucs3721@outlook.com>
2022-04-30 22:36:44 +0530

    Use `Mantine/AppShell` for layouting index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,36 @@
-import { NextPage } from "next";
-import { ModalsProvider } from "@mantine/modals";
-import { HomeLayout } from "../components/Layout";
+import React, { useState } from "react";
+import { AppShell, Navbar, MediaQuery, Burger, useMantineTheme, createStyles } from "@mantine/core";
+import { HeaderLogo } from "../components/Header";
+import { SideBar } from "../components/SideBar";
+import { MainContent } from "../components/Layout/Content";
 
-const Home: NextPage = () => {
+const useStyles = createStyles({
+	sidenavbar: {
+		backgroundColor: "hsl(240, 0%, 10%)",
+		borderRight: "1px solid hsl(0, 0%, 0%)",
+	},
+});
+
+export default function AppShellDemo() {
+	const { classes } = useStyles();
+	const theme = useMantineTheme();
+	const [opened, setOpened] = useState(false);
 	return (
-		<>
-			<ModalsProvider>
-				<HomeLayout />
-			</ModalsProvider>
-		</>
+		<AppShell
+			navbarOffsetBreakpoint="sm"
+			fixed
+			navbar={
+				<Navbar
+					p="md"
+					hiddenBreakpoint="sm"
+					hidden={!opened}
+					width={{ sm: 120, lg: 230 }}
+					className={classes.sidenavbar}>
+					<HeaderLogo />
+					<SideBar />
+				</Navbar>
+			}>
+			<MainContent />
+		</AppShell>
 	);
-};
-
-export default Home;
+}
