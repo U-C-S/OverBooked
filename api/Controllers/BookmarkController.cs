380da50aa9337fbdaaca71102e153f9c5bd6f02c
U-C-S <ucs3721@outlook.com>
2022-05-07 18:36:03 +0530

    add `api/bookmark/createbookmarks` api

--- api/Controllers/BookmarkController.cs
+++ api/Controllers/BookmarkController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using OverbookedAPI.Data.Models;
 
 namespace OverbookedAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class BookmarkController : ControllerBase
@@ -23,7 +25,7 @@ namespace OverbookedAPI.Controllers
 
 
         [HttpPost("createdir")]
-        public async Task<ActionResult<Directory>> CreateDir([FromQuery] string dir )
+        public async Task<ActionResult<Directory>> CreateDir([FromQuery] string dir)
         {
             var dirx = new Directory(dir);
             _context.Directoriex.Add(dirx);
@@ -35,8 +37,27 @@ namespace OverbookedAPI.Controllers
         [HttpGet("getdirs")]
         public async Task<ActionResult<Directory>> GetDirs()
         {
-            var dir = _context.Directoriex.ToList();
+            var dir = await _context.Directoriex.ToListAsync();
             return Ok(dir);
+        }
+
+        [HttpPost("createbookmark")]
+        public async Task<ActionResult<Bookmark>> CreateBookmark([FromQuery] string name, [FromQuery] string url, [FromQuery] string? dir)
+        {
+            var dirx = await _context.Directoriex.FirstOrDefaultAsync(x => x.Name == dir); //create a directory to store bookmarks quickly (name - Faststore, its a premitive directory)
+            if (dirx == null)
+            {
+                return NotFound("Directory not found");
+            }
+            var bookmark = new Bookmark()
+            {
+                Name = name,
+                Url = url,
+                ParentDir = dirx
+            };
+            _context.Bookmarkx.Add(bookmark);
+            await _context.SaveChangesAsync();
+            return Ok("Bookmark created");
         }
     }
 }
