c75d15686919f6409e89ca9e0f948e598c5a2545
U-C-S <ucs3721@outlook.com>
2022-05-07 15:07:21 +0530

    Delete `Link` Model

--- api/Data/OverbookedDbContext.cs
+++ api/Data/OverbookedDbContext.cs
@@ -7,18 +7,15 @@ public class OverbookedDbContext : DbContext
 {
     public OverbookedDbContext(DbContextOptions<OverbookedDbContext> options) : base(options) { }
 
-    // public DbSet<User> Users { get; set; }
-    public DbSet<Link> Links { get; set; }
     public DbSet<Profile> Profiles { get; set; }
-
     public DbSet<Bookmark> Bookmarkx { get; set; }
     public DbSet<Directory> Directoriex { get; set; }
     public DbSet<DirInDir> DirDirChild { get; set; }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseNpgsql("Host=my_host;Database=my_db;Username=my_user;Password=my_pw");
-    //}
+
+    protected override void OnModelCreating(ModelBuilder rules){
+        rules.Entity<Directory>();
+    }
 }
 
 
